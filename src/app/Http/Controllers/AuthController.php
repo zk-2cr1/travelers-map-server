bc873e036da1aff63e8243d9650b6b4f4b22769a
zk-2cr1 <108575017+zk-2cr1@users.noreply.github.com>
2023-09-24 15:35:23 +0900

    feat:✨ ユーザー登録

---
+++ src/app/Http/Controllers/AuthController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+        $json = [
+            'data' => $user
+        ];
+        return response()->json($json, Response::HTTP_OK);
+    }
+}
